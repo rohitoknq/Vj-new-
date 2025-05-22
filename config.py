0f7627ee4c3680347246c80b72a173eb77d6eefb
rohitoknq <rohitknow12@gmail.com>
2025-05-22 12:52:53 +0530

    Fainal ga

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002656509343"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002334590710"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
